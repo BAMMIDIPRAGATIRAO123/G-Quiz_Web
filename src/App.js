1dcaa6c94448b752bbce12c67a011c2ba156da8f
Ashik1103 <hajaashik11032002@gmail.com>
2021-05-16 18:28:30 +0530

    Quiz/Leaderboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Home from './components/Home/Home';
+import Home from '../src/component/Home/Home';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';  
 import {
    BrowserRouter as Router,
@@ -9,13 +9,24 @@ import {
 } from "react-router-dom";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
-import Header from "./components/Landing/Header";
+import Javascript from "../src/component/Topic/Javascript";
+import Header from "../src/component/Landing/Header";
 import {AuthProvider} from "../src/Auth";
-import Login from "./components/Landing/Login";
-import Signup from "./components/Landing/Signup";
-import About from "./components/About/About";
-import Feedback from './components/Feedback/Feedback';
-import comment from './components/Feedback/comment';
+import Login from "../src/component/Landing/Login";
+import Signup from "../src/component/Landing/Signup";
+import Python from './component/Topic/Python';
+import REACT from './component/Topic/REACT';
+import CSS from './component/Topic/CSS';
+import Bootstrap from './component/Topic/Bootstrap';
+import JSQuiz from '../src/component/Quiz/JSQuiz';
+import PythonQuiz from '../src/component/Quiz/PythonQuiz';
+import About from "../src/component/About/About";
+import board from "../src/component/Leaderboard/board";
+import REACTQuiz from "../src/component/Quiz/REACTQuiz";
+import BootstrapQuiz from "../src/component/Quiz/BootstrapQuiz";
+import CSSQuiz from "../src/component/Quiz/CSSQuiz";
+import Feedback from './component/Feedback/Feedback';
+import comment from './component/Feedback/comment';
 
 const theme = createMuiTheme({
   palette: {
@@ -46,6 +57,17 @@ function App() {
                      <Route exact path="/Login" component={Login}></Route>
                      <Route exact path="/Signup" component={Signup}></Route>
                      <Route exact path="/Home" component={Home}></Route>
+                     <Route exact path="/board" component={board}></Route>
+                     <Route exact path="/JSQuiz" component={JSQuiz}></Route>
+                     <Route exact path="/PythonQuiz" component={PythonQuiz}></Route>
+                     <Route exact path="/REACTQuiz" component={REACTQuiz}></Route>
+                     <Route exact path="/CSSQuiz" component={CSSQuiz}></Route>
+                     <Route exact path="/BootstrapQuiz" component={BootstrapQuiz}></Route>
+                     <Route exact path="/Javascript" component={Javascript}></Route>
+                     <Route exact path="/Python" component={Python}></Route>
+                     <Route exact path="/REACT" component={REACT}></Route>
+                     <Route exact path="/CSS" component={CSS}></Route>
+                     <Route exact path="/Bootstrap" component={Bootstrap}></Route>
                      <Route exact path="/About" component={About}></Route>
                      <Route exact path="/Feedback" component={Feedback}></Route>
                      <Route exact path="/comment" component={comment}></Route>
@@ -53,6 +75,7 @@ function App() {
                </Box>
             </Container>
          </Router>
+         <div className="landing"></div>
       </MuiThemeProvider>
    </AuthProvider>
   );
